7bc02d21f81cf5639271aa5126157e35af35ab03
haoran <thresh847@163.com>
2025-04-15 15:53:43 +0800

    docs: add test md

--- .source/index.ts
+++ .source/index.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck -- skip type checking
+import * as docs_1 from "../content/docs/index2.mdx?collection=docs&hash=1744697231443"
 import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1744697231443"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/index.mdx"}, data: docs_0 }], [{ info: {"path":"meta.json","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/meta.json"}, data: {"title":"Ran Docs","pages":["index","index2","react","ran-motion"],"root":true} }])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"index2.mdx","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/index2.mdx"}, data: docs_1 }], [{ info: {"path":"meta.json","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/meta.json"}, data: {"title":"Ran Docs","pages":["index","index2","react","ran-motion"],"root":true} }])
