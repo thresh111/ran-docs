952886c1cdc009706618a90879c24a850d8d2477
haoran <thresh847@163.com>
2025-04-16 19:32:57 +0800

    docs: update docs

--- .source/index.ts
+++ .source/index.ts
@@ -1,6 +1,9 @@
 // @ts-nocheck -- skip type checking
-import * as docs_1 from "../content/docs/index2.mdx?collection=docs&hash=1744697231443"
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1744697231443"
+import * as docs_4 from "../content/docs/react/scheduler.mdx?collection=docs&hash=1744791348953"
+import * as docs_3 from "../content/docs/react/event.mdx?collection=docs&hash=1744791348953"
+import * as docs_2 from "../content/docs/javascript/reverse.mdx?collection=docs&hash=1744791348953"
+import * as docs_1 from "../content/docs/javascript/promise.mdx?collection=docs&hash=1744791348953"
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1744791348953"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"index2.mdx","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/index2.mdx"}, data: docs_1 }], [{ info: {"path":"meta.json","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/meta.json"}, data: {"title":"Ran Docs","pages":["index","index2","react","ran-motion"],"root":true} }])
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"javascript/promise.mdx","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/javascript/promise.mdx"}, data: docs_1 }, { info: {"path":"javascript/reverse.mdx","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/javascript/reverse.mdx"}, data: docs_2 }, { info: {"path":"react/event.mdx","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/react/event.mdx"}, data: docs_3 }, { info: {"path":"react/scheduler.mdx","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/react/scheduler.mdx"}, data: docs_4 }], [{ info: {"path":"meta.json","absolutePath":"/Users/haoran/Desktop/thresslee-docs/content/docs/meta.json"}, data: {"title":"Ran Docs","pages":["index","react","javascript"],"root":true} }])
--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { createMDX } from 'fumadocs-mdx/next';
 const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: false,
+  // devIndicators: false,
 };
 
 export default withMDX(nextConfig);
