3daa54f6847299f62d5aba2228b0e78221147065
haoran <thresh847@163.com>
2025-04-21 13:54:40 +0800

    docs: update docs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,12 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // devIndicators: false,
+
+  images: {
+    remotePatterns: [
+      new URL('https://xiejie-typora.oss-cn-chengdu.aliyuncs.com/**'),
+    ],
+  },
 };
 
 export default withMDX(nextConfig);
